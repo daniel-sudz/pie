4e64f712cf2a97074f8c985928a29f04aa002132
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-10-06 19:13:52 -0400

    work from other day

--- miniproject3/src/main.cpp
+++ miniproject3/src/main.cpp
@@ -10,9 +10,10 @@
 
 /* GLOBAL STATE */
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor motor_left = *AFMS.getMotor(1);
-Adafruit_DCMotor motor_right = *AFMS.getMotor(2);
+Adafruit_DCMotor motor_left = *AFMS.getMotor(2);
+Adafruit_DCMotor motor_right = *AFMS.getMotor(1);
 
+float sensor_baseline = 0;
 float left_sensor = 0;
 float right_sensor = 0;
 float last_serial_time = millis();
@@ -23,7 +24,7 @@ float last_serial_command_time = millis();
 /* Based on implementation from https://www.wescottdesign.com/articles/pid/pidWithoutAPhd.pdf */
 struct PidState {
   // proportional
-  float proportional_gain = 50;
+  float proportional_gain = 400;
   // integral
   float integrator_state = 0;
   float integrator_min = 0;
@@ -58,8 +59,8 @@ PidState rightPID;
 
 /* Reads the onboard sensors and saves them to global state */
 void read_sensor_readings() {
-  left_sensor = analogRead(A0);
-  right_sensor = analogRead(A1);
+  left_sensor = analogRead(A1);
+  right_sensor = analogRead(A0);
 }
 
 /* Send the sensor readings over serial */
@@ -84,29 +85,46 @@ void send_commands(float left_command, float right_command) {
 void setup() {
   // start the serial port, must match the server!!!
   Serial.begin(115200);
+
+  // start the motor controller 
+  AFMS.begin();
+
+  // disable the default full-throttle
+  motor_left.setSpeed(0);
+  motor_right.setSpeed(0);
+  motor_left.run(FORWARD);
+  motor_right.run(FORWARD);
+
+  // figure out the reflective baseline
+  read_sensor_readings();
+  sensor_baseline = (left_sensor + right_sensor) / 2;
 }
 
 void loop() {
   // reads down to zero when black and around 1000 when white
-  float no_tape_reading = 200;
   read_sensor_readings();
 
   // send back debug info
   send_sensor_readings();
 
   // update the PID
-  float left_pid_res = leftPID.update(left_sensor - no_tape_reading, left_sensor);
-  float right_pid_res = rightPID.update(right_sensor - no_tape_reading, right_sensor);
+  float left_pid_res = leftPID.update(left_sensor - sensor_baseline, left_sensor);
+  float right_pid_res = rightPID.update(right_sensor - sensor_baseline, right_sensor);
 
   // send the motor commands
   // range is [0 - 65,536]
+  float speed_scale_factor = 0.005;
   float max_command = UINT16_MAX;
-  float left_command = max(min(max_command - left_pid_res, max_command), 0.0);  
-  float right_command = max(min(max_command - right_pid_res, max_command), 0.0);
+  float left_command = max((min(max_command - left_pid_res, max_command) * speed_scale_factor * 1.2), 0.0);  
+  float right_command = max((min(max_command - right_pid_res, max_command) * speed_scale_factor), 0.0);
 
   send_commands(left_command, right_command);
 
-  //motor_left.setSpeedFine(left_command);
-  //motor_right.setSpeedFine(right_command);
-
+  // RUN!!! :)
+  
+  motor_left.setSpeedFine(left_command);
+  motor_right.setSpeedFine(right_command);
+  motor_left.run(FORWARD);
+  motor_right.run(FORWARD);
+  
 }
