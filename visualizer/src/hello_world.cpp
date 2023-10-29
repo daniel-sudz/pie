1aa034746c8605e20a30daf23b0e6ecfa8afaaa0
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-10-29 14:23:28 -0400

    something is displaying

--- visualizer/src/hello_world.cpp
+++ visualizer/src/hello_world.cpp
@@ -1,6 +1,8 @@
 #include "AudioFile.h"
 #include<iostream>
 #include <string>
+#include <thread> 
+#include <chrono>
 
 #include "../out/_deps/sdl2-src/include/SDL.h"
 
@@ -19,11 +21,22 @@ struct RawAudioPlayer {
       std::cerr << err << std::endl;
       exit(1);
     }
+
+    /* For some reason SDL2 eats up Ctrl+C events which is super annoying */
+    static void process_sdl2_system_events() {
+      SDL_Event event;  
+          while (SDL_PollEvent(&event)) {
+            switch (event.type)  {
+              case SDL_QUIT:                 // window close icon
+                exit_with_error("ctrl+C hit");
+              }
+      }
+    }
   
   public: 
     RawAudioPlayer() {
         // Init SDL2 audio
-        if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_NOPARACHUTE) < 0) {
           RawAudioPlayer::exit_with_error("Failed to call SDL_INIT_AUDIO");
         }
 
@@ -41,18 +54,23 @@ struct RawAudioPlayer {
         }
 
         // Start audio playback 
-        SDL_PauseAudio(0);
+        SDL_PauseAudioDevice(output_device, 0);
+
+        // Ouput info
+        std::cout << "Successfully initialized audio output" << std::endl; 
     }
 
     /* Queue up audio for playback */
     void queue_audio(std::vector<float> X, std::vector<float> Y)
     {
+        // interleave the raw data for the two channels
         std::vector<float> channel_interleave;
         for (int i = 0; i < X.size();i ++) {
             channel_interleave.push_back(X[i]);
             channel_interleave.push_back(Y[i]);
         }
         SDL_QueueAudio(output_device, channel_interleave.data(), channel_interleave.size() * sizeof(channel_interleave[0]));
+        RawAudioPlayer::process_sdl2_system_events();
     }
 };
 
@@ -60,6 +78,12 @@ int main(){
   AudioFile<double> audioFile;
 
   RawAudioPlayer audio_player;
+  while (true) {
+      audio_player.queue_audio(std::vector<float>(1000, 10), std::vector<float>(1000, 10));
+      audio_player.queue_audio(std::vector<float>(1000, -10), std::vector<float>(1000, -10));
+      // don't burn the CPU too much
+      //std::this_thread::sleep_for (std::chrono::milliseconds(10));
+  }
 
   return 0;
 }
