5fd845f88b88a016345ef43684f118d44d9a7ec1
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-11-12 18:41:13 -0500

    I have a headache now

--- visualizer/src/audio/player.hpp
+++ visualizer/src/audio/player.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../../thirdparty/portaudio/include/portaudio.h"
-#include "./open_stream.hpp"
+#include "./util.hpp"
 
 namespace audio {
 
@@ -19,28 +19,55 @@ namespace audio {
         /* The stream for this player. Do not modify yourself. */
         PaStream* stream;
 
+        inline static bool PORTAUDIO_INITIALIZED = false;
+
         static int __streamCallback(const void* input, void* output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData) {
             return T::streamCallback((float*)input, (float*)output, frameCount, timeInfo, statusFlags, userData);
         }
 
        public:
         Player() {
-            audio::open_stream(stream, __streamCallback, this);
+            if (!PORTAUDIO_INITIALIZED) {
+                audio::portaudio_initialize();
+                PORTAUDIO_INITIALIZED = true;
+            }
+            printf("Value:  %p\n", stream);
+            audio::portaudio_open_stream(&stream, __streamCallback, this);
+            printf("Value:  %p\n", stream);
+            audio::portaudio_start_stream(stream);
         }
     };
 
     /* Sample player that just plays the same note */
     struct DemoPlayer : public Player<DemoPlayer> {
-        int note_to_play = 1;
+        float left_phase = 0;
+        float right_phase = 0;
+
         static int streamCallback(const float* input, float* output, unsigned long frameCount, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData) {
             /* Get a handle to ourselves */
             DemoPlayer* self = (DemoPlayer*)userData;
 
             /* Fill the buffer */
             for (int i = 0; i < frameCount; i++) {
-                output[i * 2] = self->note_to_play;        // Set left sound
-                output[(i * 2) + 1] = self->note_to_play;  // Set right sound
+                output[i * 2] = self->left_phase;         // Set left sound
+                output[(i * 2) + 1] = self->right_phase;  // Set right sound
             }
+
+            /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+            self->left_phase += 0.01f;
+
+            /* When signal reaches top, drop back down. */
+            if (self->left_phase >= 1.0f) {
+                self->left_phase -= 2.0f;
+            }
+
+            /* higher pitch so we can distinguish left and right. */
+            self->right_phase += 0.03f;
+            if (self->right_phase >= 1.0f) {
+                self->right_phase -= 2.0f;
+            }
+
+            std::cerr << "call: " << frameCount << std::endl;
 
             /* All good */
             return 0;
---
+++ visualizer/src/bin/demo_player.cpp
@@ -0,0 +1,6 @@
+#include "../audio/player.hpp"
+
+int main() {
+    audio::DemoPlayer player;
+    Pa_Sleep(1000 * 100);
+}
