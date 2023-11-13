3e3712cb015255c3e4ad64e2c93bab71e254d63b
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-11-13 16:04:43 -0500

    update baud rate and format

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -6,7 +6,6 @@ const int pot1 = A1;
 int pot1_val = 0;
 int pot0_val = 0;
 
-
 const int startPin = 12;
 const int endPin = 13;
 
@@ -14,27 +13,26 @@ bool startPressed = 0;
 bool endPressed = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(pot0, INPUT);
-  pinMode(pot1, INPUT);
-  pinMode(startPin, INPUT);
-  pinMode(endPin, INPUT);
+    Serial.begin(115200);
+    pinMode(pot0, INPUT);
+    pinMode(pot1, INPUT);
+    pinMode(startPin, INPUT);
+    pinMode(endPin, INPUT);
 }
 
 void loop() {
-  startPressed = digitalRead(startPin);
-  endPressed  = digitalRead(endPin);
-  pot0_val = 1023 - analogRead(pot0);
-  pot1_val = 1023 - analogRead(pot1);
-  Serial.println("pot0");
-  Serial.print(pot0_val);
-  Serial.println("pot1");
-  Serial.print(pot1_val);
-  if (startPressed == 1) {
-    Serial.println("START");
-  }
-  if (endPressed == 1) {
-    Serial.println("END");
-  }
-  
+    startPressed = digitalRead(startPin);
+    endPressed = digitalRead(endPin);
+    pot0_val = 1023 - analogRead(pot0);
+    pot1_val = 1023 - analogRead(pot1);
+    Serial.println("POT0");
+    Serial.println(pot0_val);
+    Serial.println("POT1");
+    Serial.println(pot1_val);
+    if (startPressed == 1) {
+        Serial.println("START");
+    }
+    if (endPressed == 1) {
+        Serial.println("END");
+    }
 }
