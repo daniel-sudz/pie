5e89aa2d174965956a5b61c55a60446fa65afc9a
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-12-06 20:35:31 -0500

    handle case where no note is being pressed (#21)

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,17 +2,13 @@
 
 // put function declarations here:
 
-const int output0 = 11;
-const int output1 = 10;
-const int output2 = 9;
-const int output3 = 8;
+// input MUX pins
+const int mux_input_pins[] = {2, 3, 4, 5, 6, 7};
+const int mux_num_input = sizeof(mux_input_pins) / sizeof(mux_input_pins[0]);
 
-const int input0 = 2;
-const int input1 = 3;
-const int input2 = 4;
-const int input3 = 5;
-const int input4 = 6;
-const int input5 = 7;
+// output MUX pins
+const int mux_output_pins[] = {11, 10, 9, 8};
+const int mux_num_output = sizeof(mux_output_pins) / sizeof(mux_output_pins[0]);
 
 const int pot0 = A0;
 const int pot1 = A1;
@@ -26,27 +22,23 @@ const int endPin = 13;
 bool startPressed = 0;
 bool endPressed = 0;
 
-const char* notes0[4] = {"130.81", "155.56", "185.00", "220.00"};
-const char* notes1[4] = {"138.59", "164.81", "196.00", "233.08"};
-const char* notes2[4] = {"146.83", "174.61", "207.65", "246.94"};
-const char* notes3[4] = {"261.63", "311.13", "369.99", "440.00"};
-const char* notes4[4] = {"277.18", "329.63", "392.00", "466.16"};
-const char* notes5[4] = {"293.66", "349.23", "415.30", "493.88"};
+// frequencies for notes on our keyboard
+const char* notes[mux_num_input][mux_num_output] = {
+    {"130.81", "155.56", "185.00", "220.00"},
+    {"138.59", "164.81", "196.00", "233.08"},
+    {"146.83", "174.61", "207.65", "246.94"},
+    {"261.63", "311.13", "369.99", "440.00"},
+    {"277.18", "329.63", "392.00", "466.16"},
+    {"293.66", "349.23", "415.30", "493.88"},
+};
 
-// put function definitions here:
-bool readPin(int inputPin) {
-    if (digitalRead(inputPin) == 'HIGH') {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-void printNote(bool* inputArray, const char** noteArray) {
+// figures out which note is being pressed based on mux state
+void printNote(bool* inputArray, const char** noteArray, bool& note_printed) {
     for (int i = 0; i < 4; i++) {
         if (inputArray[i] == 1) {
             Serial.print("KEYBOARDNOTE");
             Serial.println(noteArray[i]);
+            note_printed = true;
         }
     }
 }
@@ -54,78 +46,51 @@ void printNote(bool* inputArray, const char** noteArray) {
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    pinMode(output0, OUTPUT);
-    pinMode(output1, OUTPUT);
-    pinMode(output2, OUTPUT);
-    pinMode(output3, OUTPUT);
 
-    pinMode(input0, INPUT);
-    pinMode(input1, INPUT);
-    pinMode(input2, INPUT);
-    pinMode(input3, INPUT);
-    pinMode(input4, INPUT);
-    pinMode(input5, INPUT);
+    // configure mux output pins
+    for (int i = 0; i < mux_num_output; i++) {
+        pinMode(mux_output_pins[i], OUTPUT);
+    }
 
+    // configure mux input pins
+    for (int i = 0; i < mux_num_input; i++) {
+        pinMode(mux_input_pins[i], INPUT);
+    }
+
+    // configure potentiometer pins
     pinMode(pot0, INPUT);
     pinMode(pot1, INPUT);
+
+    // configure start/stop pins
     pinMode(startPin, INPUT);
     pinMode(endPin, INPUT);
 }
 
 void loop() {
-    // ------ MUX ------
-    bool I0[4];
-    bool I1[4];
-    bool I2[4];
-    bool I3[4];
-    bool I4[4];
-    bool I5[4];
-    // Output 0
-    digitalWrite(output0, HIGH);
-    I0[0] = digitalRead(input0);
-    I1[0] = digitalRead(input1);
-    I2[0] = digitalRead(input2);
-    I3[0] = digitalRead(input3);
-    I4[0] = digitalRead(input4);
-    I5[0] = digitalRead(input5);
-    digitalWrite(output0, LOW);
-    // Output 1
-    digitalWrite(output1, HIGH);
-    I0[1] = digitalRead(input0);
-    I1[1] = digitalRead(input1);
-    I2[1] = digitalRead(input2);
-    I3[1] = digitalRead(input3);
-    I4[1] = digitalRead(input4);
-    I5[1] = digitalRead(input5);
-    digitalWrite(output1, LOW);
-    // Output 2
-    digitalWrite(output2, HIGH);
-    I0[2] = digitalRead(input0);
-    I1[2] = digitalRead(input1);
-    I2[2] = digitalRead(input2);
-    I3[2] = digitalRead(input3);
-    I4[2] = digitalRead(input4);
-    I5[2] = digitalRead(input5);
-    digitalWrite(output2, LOW);
-    // Output 3
-    digitalWrite(output3, HIGH);
-    I0[3] = digitalRead(input0);
-    I1[3] = digitalRead(input1);
-    I2[3] = digitalRead(input2);
-    I3[3] = digitalRead(input3);
-    I4[3] = digitalRead(input4);
-    I5[3] = digitalRead(input5);
-    digitalWrite(output3, LOW);
+    /* ------------------------------------  MUX ------------------------------------  */
+    bool mux_input_read[5][4] = {};
 
-    // Print notes from boolean arrays
-    printNote(I0, notes0);
-    printNote(I1, notes1);
-    printNote(I2, notes2);
-    printNote(I3, notes3);
-    printNote(I4, notes4);
-    printNote(I5, notes5);
+    // read the mux
+    for (int mux_output = 0; mux_output < mux_num_output; mux_output++) {
+        digitalWrite(mux_output_pins[mux_output], HIGH);
+        for (int mux_input = 0; mux_input < mux_num_input; mux_input++) {
+            mux_input_read[mux_input][mux_output] = digitalRead(mux_input_pins[mux_input]);
+        }
+        digitalWrite(mux_output_pins[mux_output], LOW);
+    }
 
-    // ------ etchasketch ------
+    // print note being pressed out from the mux state
+    bool note_printed = false;
+    for (int mux_input = 0; mux_input < mux_num_input; mux_input++) {
+        printNote(mux_input_read[mux_input], notes[mux_input], note_printed);
+    }
+    if (!note_printed) {
+        Serial.println("KEYBOARDNOTE0.00");
+    }
+
+    /* ------------------------------------  MUX ------------------------------------  */
+
+    /* ------------------------------------ Etch-a-Sketch ------------------------------ */
     startPressed = digitalRead(startPin);
     endPressed = digitalRead(endPin);
     pot0_val = 1023 - analogRead(pot0);
@@ -142,4 +107,5 @@ void loop() {
     if (endPressed == 1) {
         Serial.println("END");
     }
+    /* ------------------------------------ Etch-a-Sketch ------------------------------ */
 }
